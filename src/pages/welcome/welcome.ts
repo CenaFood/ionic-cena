1eb4d63e577ad76d8c7cf00e503627a84383d7ff
Philipp Lüthi <philipp@luethi.rocks>
2018-06-29 14:10:03 +0200

    Welcome page like described in:  https://auth0.com/blog/ionic-2-authentication-how-to-secure-your-mobile-app-with-jwt/

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {Headers, Http} from "@angular/http";
+import {JwtHelper} from "angular2-jwt";
+import {Storage} from "@ionic/storage";
+import {AuthService} from "../../app/services/auth/auth";
+import 'rxjs/add/operator/map';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
@@ -14,12 +19,68 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'welcome.html',
 })
 export class WelcomePage {
+  private LOGIN_URL = "http://localhost:3001/sessions/create";
+  private SIGNUP_URL = "http://localhost:3001/users";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  auth: AuthService;
+
+  // When the page loads, we want the Login segment to be selected
+  authType: string = "login";
+
+  // We need to set the content type for the server
+  contentHeader = new Headers({"Content-Type": "application/json"});
+  error: string;
+  jwtHelper = new JwtHelper();
+  user: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http, private storage: Storage) {
+    this.auth = AuthService;
+
+    storage.ready().then(() => {
+      storage.get('profile').then(profile => {
+        this.user = JSON.parse(profile);
+      }).catch(console.log);
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WelcomePage');
   }
+
+
+  authenticate(credentials) {
+    this.authType == 'login' ? this.login(credentials) : this.signup(credentials);
+  }
+
+  login(credentials) {
+    this.http.post(this.LOGIN_URL, JSON.stringify(credentials), { headers: this.contentHeader })
+      .map(res => res.json())
+      .subscribe(
+        data => this.authSuccess(data.id_token),
+        err => this.error = err
+      );
+  }
+
+  signup(credentials) {
+    this.http.post(this.SIGNUP_URL, JSON.stringify(credentials), { headers: this.contentHeader })
+      .map(res => res.json())
+      .subscribe(
+        data => this.authSuccess(data.id_token),
+        err => this.error = err
+      );
+  }
+
+  logout() {
+    this.storage.remove('token');
+    this.user = null;
+  }
+
+  authSuccess(token) {
+    this.error = null;
+    this.storage.set('token', token);
+    this.user = this.jwtHelper.decodeToken(token).username;
+    this.storage.set('profile', this.user);
+  }
+}
 
 }
