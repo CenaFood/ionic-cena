3bf8da648fcfa62bb86566e7269ac8ee96fced02
Philipp Lüthi <philipp@luethi.rocks>
2018-07-03 18:03:29 +0200

    Get / Post API Calls with auth

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { Headers } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthProvider } from '../auth/auth';
 
 /*
   Generated class for the ApiProvider provider.
@@ -10,8 +12,33 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ApiProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello ApiProvider Provider');
+  contentHeader: HttpHeaders;
+
+  private readonly API_URL = "https://cenaswiper.luethi.rocks/";
+  
+  constructor(public http: HttpClient, private auth: AuthProvider) {
+    this.contentHeader = new HttpHeaders();
+    this.contentHeader.append('Accept','application/json');
+    this.contentHeader.append('content-type','application/json');
+    console.log('Hello ApiProvider Generated');
   }
 
+  private ApiGet(route: string){
+    this.auth.addAuthorizeHeader(this.contentHeader);
+    let getUrl = new URL(route,this.API_URL)
+    return this.http.get(
+      getUrl.toString(), 
+      {headers: this.contentHeader})
+      .toPromise();
+  }
+
+  private ApiPost(route: string, payload: object){
+    this.auth.addAuthorizeHeader(this.contentHeader);
+    let postUrl = new URL(route,this.API_URL);
+    return this.http.post(
+      postUrl.toString(), 
+      payload,
+      {headers: this.contentHeader})
+      .toPromise();
+  }
 }
