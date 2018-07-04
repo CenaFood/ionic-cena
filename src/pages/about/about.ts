333301b07bf183d10146f4d471d54afa363a471c
Philipp Lüthi <philipp@luethi.rocks>
2018-07-04 18:06:44 +0200

    Lock app to Welcome page on logout

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
+import { WelcomePage } from './../welcome/welcome';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, App } from 'ionic-angular';
+import { AuthProvider } from '../../providers/auth/auth';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +9,12 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public auth: AuthProvider, private app: App) {
 
   }
 
+  private logout(){
+    this.auth.logout();
+    this.app.getRootNav().setRoot(WelcomePage);
+  }
 }
